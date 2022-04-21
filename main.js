004ff0ad4c35f6602c950f72b5395f04b55014cc
Ryu☆ <76149388+unknowkzr@users.noreply.github.com>
2022-04-21 14:31:03 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -35,16 +35,16 @@ global.timestamp = {
 const PORT = process.env.PORT || process.env.SERVER_PORT || 3000
 
 global.opts = new Object(yargs(process.argv.slice(2)).exitProcess(false).parse())
-opts['db'] = 'mongodb+srv://ryubot:lunoxlance@cluster0.qupcb.mongodb.net/Cluster0?retryWrites=true&w=majority'
 // console.log({ opts })
 global.prefix = new RegExp('^[' + (opts['prefix'] || '‎xzXZ/i!#$,%+£¢€¥^°=¶∆×÷π√✓©®:;?&.\\-').replace(/[|\\{}()[\]^$+*?.\-\^]/g, '\\$&') + ']')
 
-global.db = new Low(
+/*global.db = new Low(
   /https?:\/\//.test(opts['db'] || '') ?
     new cloudDBAdapter(opts['db']) : /mongodb/.test(opts['db']) ?
       new mongoDB(opts['db']) :
       new JSONFile(`${opts._[0] ? opts._[0] + '_' : ''}database.json`)
-)
+)*/
+global.db = new Low(new mongoDB('mongodb+srv://ryubot:lunoxlance@cluster0.qupcb.mongodb.net/Cluster0?retryWrites=true&w=majority'))
 //global.DATABASE = global.db  Backwards Compatibility
 global.loadDatabase = async function loadDatabase() {
   if (global.db.READ) return new Promise((resolve) => setInterval(function () { (!global.db.READ ? (clearInterval(this), resolve(global.db.data == null ? global.loadDatabase() : global.db.data)) : null) }, 1 * 1000))
